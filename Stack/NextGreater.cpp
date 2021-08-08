374100142b8f149d71b27399f8e51a0863a4093c
Pranav Shrivastava <63885865+callmepranavv@users.noreply.github.com>
2021-08-09 04:55:49 +0530

    Update NextGreater.cpp

--- Stack/NextGreater.cpp
+++ Stack/NextGreater.cpp
@@ -1,26 +1,33 @@
+// Function to return Next larger elements
+
 class Solution
 {
     public:
     //Function to find the next greater element for each element of the array.
-    vector<long long> nextLargerElement(vector<long long> arr, int n){
-        // Your code here
+    vector<long long> nextLargerElement(vector<long long> arr, int n)
+    {
+        stack<long long > s;
+        vector<long long > res (n);
         
-       stack <long long> s;
-       
-       vector <long long> V(n);
-       
-       for(int i=n-1;i>=0;i--)
-       {
-           while(s.empty()==false && s.top() <= arr[i])
-                s.pop();
-           
-            if (s.empty ())
-                V[i] = -1;
-            else 
-                V[i] = s.top ();
+        //traversing the array from last element in backward direction.
+        for (int i = n-1; i >= 0; i--)
+        {
+            //while element at top of stack is less than or equal to
+            //current array element, we pop elements from the stack.
+            while (!s.empty () and s.top () <= arr[i])
+                s.pop ();
             
+            //if stack becomes empty, we store -1 in the answer list 
+            //else we store the top element of the stack.   
+            if (s.empty ())
+                res[i] = -1;
+            else 
+                res[i] = s.top ();
+            
+            //pushing the current array element into the stack.  
             s.push (arr[i]);
-       }
-       return V;
+        }
+        //returning the list.
+        return res;
     }
 };
